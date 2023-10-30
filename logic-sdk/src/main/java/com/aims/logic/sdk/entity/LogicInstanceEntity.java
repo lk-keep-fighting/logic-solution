07e3614d6313be948850dd78b21bfd17fe10c7b9
lk <lkllyy@126.com>
2023-10-30 22:22:20 +0800

    feat: 删除mybatis-plus关于下划线转驼峰的配置，使用TableField直接标注数据库字段

--- logic-sdk/src/main/java/com/aims/logic/sdk/entity/LogicInstanceEntity.java
+++ logic-sdk/src/main/java/com/aims/logic/sdk/entity/LogicInstanceEntity.java
@@ -1,5 +1,6 @@
 package com.aims.logic.sdk.entity;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
@@ -14,19 +15,30 @@ import java.util.Date;
 public class LogicInstanceEntity extends Model<LogicInstanceEntity> {
     @TableId
     private String id;
+    @TableField("logicId")
     private String logicId;
     private String version;
+    @TableField("bizId")
     private String bizId;
+    @TableField("nextId")
     private String nextId;
+    @TableField("nextName")
     private String nextName;
+    @TableField("paramsJson")
     private String paramsJson;
+    @TableField("varsJson")
     private String varsJson;
+    @TableField("varsJsonEnd")
     private String varsJsonEnd;
+    @TableField("returnData")
     private String returnData;
     private boolean success;
+    @TableField("isOver")
     private boolean isOver;
     private String message;
+    @TableField("messageId")
     private String messageId;
+    @TableField("serverTime")
     private Date serverTime;
     private String env;
 }
