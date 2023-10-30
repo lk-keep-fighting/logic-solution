ecdfde9051a5d5bbacb4f5ab74385290b8a61235
lk <lkllyy@126.com>
2023-10-30 22:24:31 +0800

    feat: 优化run入参的JSONObject为String，降低调用时理解三分裤的成本

---
+++ logic-sdk/src/main/java/com/aims/logic/sdk/BizLogicRunner.java
@@ -0,0 +1,73 @@
+package com.aims.logic.sdk;
+
+import com.aims.logic.runtime.contract.dto.LogicRunResult;
+import com.aims.logic.sdk.entity.LogicInstanceEntity;
+import com.aims.logic.sdk.service.LoggerService;
+import com.aims.logic.sdk.service.LogicInstanceService;
+import com.aims.logic.sdk.util.RuntimeUtil;
+import com.aims.logic.util.JsonUtil;
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class BizLogicRunner {
+    private final LoggerService logService;
+    private final LogicInstanceService insService;
+
+    @Autowired
+    public BizLogicRunner(LoggerService _logService,
+                          LogicInstanceService _insService) {
+        this.logService = _logService;
+        this.insService = _insService;
+    }
+
+    /**
+     * 通过业务标识运行
+     * @param logicId 逻辑编号
+     * @param bizId 业务唯一标识
+     * @param parsJsonString 入参json字符串
+     * @return 执行结果
+     */
+    public LogicRunResult runBiz(String logicId, String bizId, String parsJsonString) {
+        JSONObject pars = parsJsonString == null ? null : JSONObject.parseObject(parsJsonString);
+        return runBiz(logicId, bizId, pars, null);
+    }
+
+    /**
+     * 执行业务逻辑
+     * @param logicId 逻辑编号
+     * @param bizId 业务编号
+     * @param pars 入参json
+     * @param customEnv 环境变量json
+     * @return 执行结果
+     */
+    public LogicRunResult runBiz(String logicId, String bizId, JSONObject pars, JSONObject customEnv) {
+        JSONObject config = RuntimeUtil.readLogicConfig(logicId);
+        if (config == null) throw new RuntimeException("未发现指定的逻辑：" + logicId);
+        config.put("id", logicId);//自动修复文件名编号与内部配置编号不同的问题
+        JSONObject env = RuntimeUtil.getEnvJson();
+        env = JsonUtil.jsonMerge(customEnv, env);
+        String startId = null;
+        String cacheVarsJson = null;
+        if (bizId != null && !bizId.isBlank()) {
+            QueryWrapper<LogicInstanceEntity> q = new QueryWrapper<>();
+            q.allEq(Map.of("logicId", logicId, "bizId", bizId));
+            LogicInstanceEntity insEntity = insService.getOne(q);
+            cacheVarsJson = insEntity == null ? null : insEntity.getVarsJsonEnd();
+            startId = insEntity == null ? null : insEntity.getNextId();
+            if (insEntity != null && insEntity.isOver()) {
+                return new LogicRunResult().setSuccess(false).setMsg(String.format("指定的bizId:%s已完成执行，无法重复执行。", bizId));
+            }
+        }
+        var res = new com.aims.logic.runtime.logic.LogicRunner(config, env)
+                .run(startId, pars, JSON.isValid(cacheVarsJson) ? JSON.parseObject(cacheVarsJson) : null);
+        res.getLogicLog().setBizId(bizId);
+        logService.addLog(res);
+        return res;
+    }
+}
--- logic-sdk/src/main/java/com/aims/logic/sdk/LogicRunner.java
+++ logic-sdk/src/main/java/com/aims/logic/sdk/LogicRunner.java
@@ -27,41 +27,8 @@ public class LogicRunner {
         this.insService = _insService;
     }
 
-    /**
-     * 传入逻辑配置与入参执行逻辑
-     *
-     * @param logicConfig 逻辑配置
-     * @param pars        入参
-     * @return 返回参数
-     */
-//    public LogicRunResult run(JSONObject logicConfig, JSONObject pars) {
-//        return run(logicConfig, pars, null);
-//    }
-
-    /**
-     * 传入逻辑配置、入参、自定义环境变量执行逻辑
-     *
-     * @param logicConfig 逻辑配置
-     * @param pars        入参
-     * @param customEnv   自定义环境变量
-     * @return 返回参数
-     */
-//    public LogicRunResult run(JSONObject logicConfig, JSONObject pars, JSONObject customEnv) {
-//        JSONObject env = RuntimeUtil.readEnv();
-//        env = JsonUtil.jsonMerge(customEnv, env);
-//        var res = new com.aims.logic.runtime.logic.LogicRunner(logicConfig, env).run(pars);
-//        logService.addLog(res);
-//        return res;
-//    }
-
-    /**
-     * 传入逻辑编号与入参执行逻辑
-     *
-     * @param logicId 逻辑编号
-     * @param pars    入参
-     * @return 返回参数
-     */
-    public LogicRunResult run(String logicId, JSONObject pars) {
+    public LogicRunResult run(String logicId, String parsJsonString) {
+        JSONObject pars = parsJsonString == null ? null : JSONObject.parseObject(parsJsonString);
         return run(logicId, pars, null);
     }
 
@@ -82,32 +49,4 @@ public class LogicRunner {
         return res;
     }
 
-    public LogicRunResult runBiz(String logicId, String bizId, JSONObject pars) {
-        return runBiz(logicId, bizId, pars, null);
-    }
-
-    public LogicRunResult runBiz(String logicId, String bizId, JSONObject pars, JSONObject customEnv) {
-        JSONObject config = RuntimeUtil.readLogicConfig(logicId);
-        if (config == null) throw new RuntimeException("未发现指定的逻辑：" + logicId);
-        config.put("id", logicId);//自动修复文件名编号与内部配置编号不同的问题
-        JSONObject env = RuntimeUtil.getEnvJson();
-        env = JsonUtil.jsonMerge(customEnv, env);
-        String startId = null;
-        String cacheVarsJson = null;
-        if (bizId != null && !bizId.isBlank()) {
-            QueryWrapper<LogicInstanceEntity> q = new QueryWrapper<>();
-            q.allEq(Map.of("logicId", logicId, "bizId", bizId));
-            LogicInstanceEntity insEntity = insService.getOne(q);
-            cacheVarsJson = insEntity == null ? null : insEntity.getVarsJsonEnd();
-            startId = insEntity == null ? null : insEntity.getNextId();
-            if (insEntity != null && insEntity.isOver()) {
-                return new LogicRunResult().setSuccess(false).setMsg(String.format("指定的bizId:%s已完成执行，无法重复执行。", bizId));
-            }
-        }
-        var res = new com.aims.logic.runtime.logic.LogicRunner(config, env)
-                .run(startId, pars, JSON.isValid(cacheVarsJson) ? JSON.parseObject(cacheVarsJson) : null);
-        res.getLogicLog().setBizId(bizId);
-        logService.addLog(res);
-        return res;
-    }
 }
