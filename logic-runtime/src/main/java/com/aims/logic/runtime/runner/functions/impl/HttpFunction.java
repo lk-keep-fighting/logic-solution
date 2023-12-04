6fd02a5025dd30c1c2bce9be279f3944b3299e52
lk <442969153@qq.com>
2023-12-04 17:51:56 +0800

    feat: http为/开头时默认请求当前宿主服务

--- logic-runtime/src/main/java/com/aims/logic/runtime/runner/functions/impl/HttpFunction.java
+++ logic-runtime/src/main/java/com/aims/logic/runtime/runner/functions/impl/HttpFunction.java
@@ -5,6 +5,7 @@ import com.aims.logic.contract.dto.LogicItemRunResult;
 import com.aims.logic.runtime.runner.FunctionContext;
 import com.aims.logic.runtime.runner.Functions;
 import com.aims.logic.runtime.runner.functions.HttpFunctionService;
+import com.aims.logic.util.RuntimeUtil;
 import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONObject;
 import okhttp3.*;
@@ -33,29 +34,39 @@ public class HttpFunction implements HttpFunctionService {
         Object data = Functions.get("js").invoke(ctx, itemDsl.getBody()).getData();
         var customHeaders = Functions.get("js").invoke(ctx, itemDsl.getHeaders()).getData();
         var method = itemDsl.getMethod().isEmpty() ? "post" : itemDsl.getMethod();
-        var url = Functions.get("js").invoke(ctx, itemDsl.getUrl()).getData();
+        var url = (String) Functions.get("js").invoke(ctx, itemDsl.getUrl()).getData();
+        if (url != null && url.startsWith(("/"))) {
+            url = RuntimeUtil.getUrl() + url;
+        }
         OkHttpClient client = httpClient.newBuilder()
                 .connectTimeout(Integer.parseInt(itemDsl.getTimeout()), TimeUnit.MILLISECONDS)
                 .readTimeout(Integer.parseInt(itemDsl.getTimeout()), TimeUnit.MILLISECONDS)
                 .writeTimeout(Integer.parseInt(itemDsl.getTimeout()), TimeUnit.MILLISECONDS)
                 .build();
         Map<String, String> headerMap = new HashMap<>();
-        JSONObject cusHeadersJson = (JSONObject) JSON.toJSON(customHeaders);
-        if (cusHeadersJson != null) {
-            cusHeadersJson.forEach((k, v) -> headerMap.put(k, (String) v));
+        if (customHeaders != null) {
+            if (JSON.isValidObject(customHeaders.toString())) {
+                JSONObject cusHeadersJson = (JSONObject) JSON.toJSON(customHeaders);
+                if (cusHeadersJson != null) {
+                    cusHeadersJson.forEach((k, v) -> headerMap.put(k, (String) v));
+                }
+            } else {
+                throw new RuntimeException("headers格式错误，应为json对象：" + customHeaders);
+            }
         }
+
         String jsonData = data == null ? "{}" : JSON.toJSONString(data);
-        if (!headerMap.containsKey("content-type")) {
+        if (!headerMap.containsKey("content-type")) {//默认json请求
             headerMap.put("content-type", "application/json");
         }
         Headers headers = Headers.of(headerMap);
         itemInstance.setMethod(method);
         itemInstance.setHeaders(JSONObject.from(headerMap).toJSONString());
         itemInstance.setBody(jsonData);
-        itemInstance.setUrl(url == null ? null : url.toString());
+        itemInstance.setUrl(url);
         itemInstance.setTimeout(itemDsl.getTimeout());
         Request req;
-        var reqBuilder = new Request.Builder().url((String) url).headers(headers);
+        var reqBuilder = new Request.Builder().url(url).headers(headers);
         if ("get".equalsIgnoreCase(method)) {
             req = reqBuilder.get().build();
         } else {
