67a4d9d571e523c5b12c53c8a5df522a540944f5
lk <442969153@qq.com>
2023-12-27 09:20:25 +0800

    fix: logic-ide git问题

---
+++ logic-ide/src/main/java/com/aims/logic/ide/LogicIdeApplication.java
@@ -0,0 +1,15 @@
+package com.aims.logic.ide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication(scanBasePackages = "com.aims.logic")
+//@SpringBootConfiguration
+public class LogicIdeApplication {
+
+//    public static void main(String[] args) {
+//        SpringApplication.run(LogicIdeApplication.class, args);
+//    }
+
+}
