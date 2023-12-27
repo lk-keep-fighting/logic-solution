294ba2dd2d92fda6dd4fe82df50bf14eb040e35c
lk <442969153@qq.com>
2023-12-27 16:02:11 +0800

    fix: 优化代码

--- logic-runtime/src/main/java/com/aims/logic/runtime/runner/functions/impl/JavaCodeFunction.java
+++ logic-runtime/src/main/java/com/aims/logic/runtime/runner/functions/impl/JavaCodeFunction.java
@@ -22,184 +22,87 @@ import java.util.*;
 
 @Service
 public class JavaCodeFunction implements ILogicItemFunctionRunner {
-    @Override
     public LogicItemRunResult invoke(FunctionContext ctx, Object item) {
-        var itemDsl = ((LogicItemTreeNode) item);
-//            ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        var itemDsl = (LogicItemTreeNode) item;
         try {
             var clazz = ClassLoaderUtils.loadClass(itemDsl.getUrl().trim());
-            System.out.printf("load class:%s%n", itemDsl.getUrl().trim());
+            System.out.printf("加载类：%s%n", itemDsl.getUrl().trim());
             var bodyObj = Functions.get("js").invoke(ctx, itemDsl.getBody()).getData();
             var methodName = itemDsl.getMethod().split("\\(")[0];
-            try {
-                //参数声明
-                List<ParamTreeNode> paramTreeNodes = itemDsl.getParams();
-                var paramsMap = JsonUtil.toObject((ScriptObjectMirror) bodyObj);
-                var paramsJson = JSONObject.from(paramsMap);
-                //将参数转换为数组，用于反射调用
-                List<Object> paramsArrayFromJsObj = new ArrayList<>();
-                //将参数声明转换为参数名-参数类型类的键值对，用于反射调用
-                Map<String, TypeAnnotationTreeNode> parameterTypes = new HashMap<>();
-                paramTreeNodes.forEach(p -> {
-                    parameterTypes.put(p.getName(), p.getTypeAnnotation());
-                });
-                ScriptObjectMirror paramJsObject = (ScriptObjectMirror) bodyObj;
 
-                List<Class<?>> cls = new ArrayList<>();
-                paramJsObject.forEach((k, v) -> {
-                    var paramTypeAnno = parameterTypes.get(k);
-                    Class<?> paramClass = null;
-                    ClassWrapper classWrapper = ClassWrapper.of(paramTypeAnno.getTypeNamespace());
-                    try {
-//                        var objectValue = JSONObject.parseObject(JSON.toJSONString(v), paramClass);
-//                        ObjectMapper objectMapper = new ObjectMapper();
-//                        ScriptObjectMirror objV = (ScriptObjectMirror) v;
-//                        var vJson = JSON.toJSONString(v);
-//                        var objVjson = objV.to(JSONObject.class);
-                        Object obj;
-                        if (paramTypeAnno.getTypeKind() == TypeKindEnum.generic) {
-                            if (Objects.equals(paramTypeAnno.getTypeName(), Map.class.getTypeName())) {
-//                                throw new RuntimeException("暂不支持Map类型，请使用其他对象，如JSONObject代替");
-                                var parTypes = classWrapper.getParameterizedType();
-                                var key = parTypes.get(0);
-                                var value = parTypes.get(1);
-                                var keyClazz = ClassLoaderUtils.loadClass(key.getName());
-//                                var keyIns = keyClazz.getDeclaredConstructor().newInstance();
-                                var valClazz = ClassLoaderUtils.loadClass(value.getName());
-//                                var valIns = valClazz.getDeclaredConstructor().newInstance();
-                                obj = paramsJson.getJSONObject(k).to(TypeReference.mapType(Map.class, keyClazz, valClazz));
-                                paramClass = obj.getClass();
-                                cls.add(paramClass);
-//                                Type mapType = new ParameterizedType() {
-//                                    @Override
-//                                    public Type[] getActualTypeArguments() {
-//                                        return new Type[]{keyClazz, valClazz};
-//                                    }
-//
-//                                    @Override
-//                                    public Type getRawType() {
-//                                        return Map.class;
-//                                    }
-//
-//                                    @Override
-//                                    public Type getOwnerType() {
-//                                        return null;
-//                                    }
-//                                };
-//                                var mapIns = mapType.getClass().getDeclaredConstructor().newInstance();
-//                                var targetMap = Map.copyOf((Map) mapIns);
-//                                paramClass = targetMap.getClass();
-//                                obj = JSONObject.parseObject(JSON.toJSONString(paramsJson.get(k)), paramClass);
-//                                cls.add(paramClass);
-                            } else {
-                                paramClass = ClassLoaderUtils.loadClass(classWrapper.getPackageName() + "." + classWrapper.getShortRawName());
-                                var TypeParClazz = ClassLoaderUtils.loadClass(classWrapper.getParameterizedType().get(0).getName());
-                                obj = JSONArray.parseArray(JSONObject.toJSONString(paramsJson.get(k)), TypeParClazz);
-                                cls.add(paramClass);
-                            }
-                        } else if (paramTypeAnno.getTypeKind() == TypeKindEnum.primitiveArray) {
-                            paramClass = DataType.getJavaClass(paramTypeAnno.getTypeNamespace());
-                            obj = paramsJson.getJSONArray(k).to(paramClass);
-                            cls.add(paramClass);
-                        } else if (paramTypeAnno.getTypeKind() == TypeKindEnum.primitive) {
-                            paramClass = DataType.getJavaClass(paramTypeAnno.getTypeNamespace());
-                            obj = JSONObject.parseObject(JSON.toJSONString(paramsJson.get(k)), paramClass);
+            List<Object> paramsArrayFromJsObj = new ArrayList<>();
+            List<Class<?>> cls = new ArrayList<>();
+
+            // 处理参数
+            List<ParamTreeNode> paramTreeNodes = itemDsl.getParams();
+            var paramsJson = JSONObject.from(JsonUtil.toObject((ScriptObjectMirror) bodyObj));
+
+            for (ParamTreeNode param : paramTreeNodes) {
+                var paramName = param.getName();
+                var paramTypeAnno = param.getTypeAnnotation();
+                var classWrapper = ClassWrapper.of(paramTypeAnno.getTypeNamespace());
+
+                Class<?> paramClass = null;
+                Object obj = null;
+
+                switch (paramTypeAnno.getTypeKind()) {
+                    case generic:
+                        if (paramTypeAnno.getTypeName().equals(Map.class.getTypeName())) {
+                            var parTypes = classWrapper.getParameterizedType();
+                            var key = parTypes.get(0);
+                            var value = parTypes.get(1);
+                            var keyClazz = ClassLoaderUtils.loadClass(key.getName());
+                            var valClazz = ClassLoaderUtils.loadClass(value.getName());
+                            obj = paramsJson.getJSONObject(paramName).to(TypeReference.mapType(Map.class, keyClazz, valClazz));
+                            paramClass = obj.getClass();
                             cls.add(paramClass);
                         } else {
                             paramClass = ClassLoaderUtils.loadClass(classWrapper.getPackageName() + "." + classWrapper.getShortRawName());
+                            var TypeParClazz = ClassLoaderUtils.loadClass(classWrapper.getParameterizedType().get(0).getName());
+                            obj = JSONArray.parseArray(JSONObject.toJSONString(paramsJson.get(paramName)), TypeParClazz);
                             cls.add(paramClass);
-                            obj = JSONObject.parseObject(JSON.toJSONString(paramsJson.get(k)), paramClass);
                         }
-                        paramsArrayFromJsObj.add(obj);
-                    } catch (ClassNotFoundException e) {
-                        throw new RuntimeException(e);
-                    }
-                    if (paramClass == null) {
-                        throw new RuntimeException(String.format("参数错误，参数%s在当前方法未声明", k));
-                    }
-//                    try {
-//                        if (v == null | paramClass.isPrimitive() | paramTypeAnno.getTypeKind() == TypeKindEnum.primitive | paramClass.getName().equals("java.lang.Integer") | paramClass.getName().equals("java.lang.String") | paramClass.getName().equals("java.lang.Object")) {
-//                            paramsArrayFromJsObj.add(v);
-//                        } else {
-//                            ScriptObjectMirror objV = (ScriptObjectMirror) v;
-//                            Object objectValue;
-//                            if (objV.isArray() | paramTypeAnno.getTypeKind() == TypeKindEnum.generic) {
-//                                var TypeParClazz = ClassLoaderUtils.loadClass(classWrapper.getParameterizedType().get(0).getName());
-//                                Object[] values = objV.values().toArray();
-//                                objectValue = JSONArray.from(values).toJavaList(TypeParClazz);
-//                            } else {
-//                                var parClazz = paramClass.getDeclaredConstructor().newInstance();
-//                                for (var field : parClazz.getClass().getDeclaredFields()) {
-//                                    field.setAccessible(true);
-//                                    if (objV.containsKey(field.getName())) {
-//                                        var value = objV.get(field.getName());
-//                                        if (value instanceof ScriptObjectMirror) {
-//                                            JSObject jsObject = (ScriptObjectMirror) value;
-//                                            if (jsObject.isArray()) {
-//                                                Object[] values = jsObject.values().toArray();
-//                                                var fieldProperty = paramTypeAnno.getProperties().stream().filter(a -> a.getName().equals(field.getName())).findFirst().orElse(null);
-//                                                if (fieldProperty != null) {
-//                                                    ClassWrapper fieldClassWrapper = ClassWrapper.of(fieldProperty.getTypeAnnotation().getTypeNamespace());
-//                                                    var TypeParClazz = ClassLoaderUtils.loadClass(fieldClassWrapper.getParameterizedType().get(0).getName());
-//                                                    objectValue = JSONArray.from(values).toJavaList(TypeParClazz);
-//                                                    field.set(parClazz, objectValue);
-//                                                } else {
-//                                                    throw new RuntimeException(String.format("未发现属性%s的声明！", field.getName()));
-//                                                }
-//                                            } else {
-//                                                field.set(parClazz, value);
-////                                                System.err.printf("javaCodeRunner类型未匹配，未处理的字段:%s", field.getName());
-//                                            }
-//                                        } else {
-//                                            field.set(parClazz, value);
-//                                        }
-//                                    }
-//                                }
-//                                objectValue = parClazz;
-//                            }
-//                            paramsArrayFromJsObj.add(objectValue);
-//                        }
-//                    } catch (InvocationTargetException ex) {
-//                        throw new RuntimeException(ex);
-//                    } catch (InstantiationException ex) {
-//                        throw new RuntimeException(ex);
-//                    } catch (IllegalAccessException ex) {
-//                        throw new RuntimeException(ex);
-//                    } catch (NoSuchMethodException ex) {
-//                        throw new RuntimeException(ex);
-//                    } catch (ClassNotFoundException e) {
-//                        throw new RuntimeException(e);
-//                    }
-                });
-                Method method = clazz.getDeclaredMethod(methodName, cls.toArray(new Class<?>[]{}));
-                LogicItemRunResult res = new LogicItemRunResult();
-                try {
-                    var obj = method.invoke(SpringContextUtil.getBean(clazz), paramsArrayFromJsObj.toArray());
-                    res.setData(obj);
-                } catch (InvocationTargetException invocationTargetException) {
-                    System.err.printf("执行方法%s报错%n", methodName);
-                    invocationTargetException.printStackTrace();
-                    throw new RuntimeException(invocationTargetException.getTargetException().getCause());
-                }
-                return res;
+                        break;
 
-            } catch (
-                    NoSuchMethodException e) {
-                ctx.setHasErr(true);
-                ctx.setErrMsg(String.format("未找到方法%s,请检查方法名与提交参数是否与实际一致！", methodName));
-                System.err.println(e.toString());
-                return new LogicItemRunResult().setData(e.toString());
+                    case primitiveArray:
+                        paramClass = DataType.getJavaClass(paramTypeAnno.getTypeNamespace());
+                        obj = paramsJson.getJSONArray(paramName).to(paramClass);
+                        cls.add(paramClass);
+                        break;
+
+                    case primitive:
+                        paramClass = DataType.getJavaClass(paramTypeAnno.getTypeNamespace());
+                        obj = JSONObject.parseObject(JSON.toJSONString(paramsJson.get(paramName)), paramClass);
+                        cls.add(paramClass);
+                        break;
+
+                    default:
+                        paramClass = ClassLoaderUtils.loadClass(classWrapper.getPackageName() + "." + classWrapper.getShortRawName());
+                        cls.add(paramClass);
+                        obj = JSONObject.parseObject(JSON.toJSONString(paramsJson.get(paramName)), paramClass);
+                }
+
+                if (paramClass == null) {
+                    throw new RuntimeException(String.format("参数错误，当前方法未声明参数%s", paramName));
+                }
+
+                paramsArrayFromJsObj.add(obj);
             }
-        } catch (
-                Exception e) {
+
+            Method method = clazz.getDeclaredMethod(methodName, cls.toArray(new Class<?>[]{}));
+            LogicItemRunResult res = new LogicItemRunResult();
+            try {
+                var obj = method.invoke(SpringContextUtil.getBean(clazz), paramsArrayFromJsObj.toArray());
+                res.setData(obj);
+            } catch (InvocationTargetException e) {
+                throw new RuntimeException(e.getTargetException().getCause());
+            }
+
+            return res;
+        } catch (Exception e) {
             e.printStackTrace();
             throw new RuntimeException(e.toString());
-//            ctx.setHasErr(true);
-//            ctx.setErrMsg(e.toString());
-//            System.err.println(e.toString());
-//            return new LogicItemRunResult().setData(e.toString());
         }
-
     }
 
     @Override
