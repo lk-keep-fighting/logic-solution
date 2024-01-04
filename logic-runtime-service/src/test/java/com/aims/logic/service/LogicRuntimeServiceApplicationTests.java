d8f5596de948aaac19e4d2b9beddde94fa866daa
lk <442969153@qq.com>
2024-01-04 20:08:56 +0800

    test: 配置deploy环境，用于发布前基于offline文件进行自动化测试

--- logic-runtime-service/src/test/java/com/aims/logic/service/LogicRuntimeServiceApplicationTests.java
+++ logic-runtime-service/src/test/java/com/aims/logic/service/LogicRuntimeServiceApplicationTests.java
@@ -1,6 +1,7 @@
 package com.aims.logic.service;
 
 import com.aims.logic.sdk.LogicRunnerServiceImpl;
+import com.aims.logic.service.demo.dto.TestInput;
 import com.aims.logic.service.demo.entity.TestEntity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,15 +9,14 @@ import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 @SpringBootTest
 class LogicRuntimeServiceApplicationTests {
 
-    //    @Test
+    @Test
     void contextLoads() {
+        testObjectArgs();
     }
 
     @Autowired
@@ -30,6 +30,26 @@ class LogicRuntimeServiceApplicationTests {
         Map<String, Object> pars = new HashMap<>();
         pars.put("entity", entity);
         logic.runByMap("test.tran", pars);
+    }
+
+    void testObjectArgs() {
+        TestInput entity = new TestInput();
+        entity.setI(11);
+        entity.setStr("11str");
+        entity.setArr(List.of("1", "12"));
+        TestInput entity2 = new TestInput();
+        entity2.setI(22);
+        entity2.setStr("22str");
+        entity2.setArr(List.of("21", "22"));
+        List<TestInput> entityList = new ArrayList<>();
+        entityList.add(entity);
+        entityList.add(entity2);
+        List<String> strings = List.of("1", "2");
+        var res = logic.runBizByObjectArgs("test.objectArgs", UUID.randomUUID().toString(), entityList, 1, strings);
+        System.out.println(">>>testObjectArgs--result");
+        System.out.println(res.getMsg());
+        System.out.println(res.getData());
+        System.out.println(">>>testObjectArgs--result over");
     }
 
 }
