dc5b5d9dcebf6a90e9915922193503c2e7e9e7ed
lk <442969153@qq.com>
2024-04-01 22:36:26 +0800

    feat: 0.0.16通过json-to-sql支持json结构化查询

--- logic-sdk/src/main/java/com/aims/logic/sdk/mapper/LogicInstanceMapper.java
+++ logic-sdk/src/main/java/com/aims/logic/sdk/mapper/LogicInstanceMapper.java
@@ -1,7 +1,6 @@
 package com.aims.logic.sdk.mapper;
 
 import com.aims.logic.sdk.entity.LogicInstanceEntity;
-import com.aims.logic.sdk.entity.LogicLogEntity;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
--- logic-sdk/src/main/java/com/aims/logic/sdk/service/impl/BaseServiceImpl.java
+++ logic-sdk/src/main/java/com/aims/logic/sdk/service/impl/BaseServiceImpl.java
@@ -2,12 +2,27 @@ package com.aims.logic.sdk.service.impl;
 
 import com.aims.logic.sdk.dto.FormQueryInput;
 import com.aims.logic.sdk.service.BaseService;
+import com.aims.lowcode.tools.jsontosql.dto.Query;
+import com.aims.lowcode.tools.jsontosql.service.JsonToQuerySqlService;
+import com.alibaba.fastjson2.JSONObject;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
 public class BaseServiceImpl<M extends BaseMapper<T>, T> extends ServiceImpl<M, T> implements BaseService<T> {
+
+    @Autowired
+    JsonToQuerySqlService jsonToQuerySqlService;
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     public Page<T> selectPage(FormQueryInput input) {
         Page<T> page = new Page<>(input.getPage(), input.getPageSize());
@@ -33,5 +48,21 @@ public class BaseServiceImpl<M extends BaseMapper<T>, T> extends ServiceImpl<M, 
             });
         }
         return baseMapper.selectPage(page, queryWrapper);
+    }
+
+    public Page<Map<String, Object>> selectPageByJson(String json) {
+        Query query = JSONObject.parseObject(json, Query.class);
+        var sql = jsonToQuerySqlService.toSqlByDto(query);
+        var list = jdbcTemplate.queryForList(sql);
+        var fromIdex = sql.indexOf("FROM");
+        var countSql = "SELECT COUNT(*) FROM " + sql.substring(fromIdex + 4);
+        var count = jdbcTemplate.queryForObject(countSql, Long.class);
+        var p = new Page<Map<String, Object>>(query.getPage().getNumber(), query.getPage().getSize(), count);
+        p.setRecords(list);
+        return p;
+    }
+
+    public List<Map<String, Object>> selectBySql(String sql) {
+        return jdbcTemplate.queryForList(sql);
     }
 }
