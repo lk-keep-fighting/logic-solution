79249c1d081b4b5914fe23e46a21b083eac2be95
lk <442969153@qq.com>
2024-04-01 22:36:26 +0800

    feat: 0.0.16通过json-to-sql支持json结构化查询

--- logic-sdk/src/main/java/com/aims/logic/sdk/mapper/LogicPublishedMapper.java
+++ logic-sdk/src/main/java/com/aims/logic/sdk/mapper/LogicPublishedMapper.java
@@ -1,6 +1,5 @@
 package com.aims.logic.sdk.mapper;
 
-import com.aims.logic.sdk.entity.LogicEntity;
 import com.aims.logic.sdk.entity.LogicPublishedEntity;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
--- logic-sdk/src/main/java/com/aims/logic/sdk/service/BaseService.java
+++ logic-sdk/src/main/java/com/aims/logic/sdk/service/BaseService.java
@@ -4,6 +4,12 @@ import com.aims.logic.sdk.dto.FormQueryInput;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.List;
+import java.util.Map;
+
 public interface BaseService<T> extends IService<T> {
     Page<T> selectPage(FormQueryInput input);
+
+    Page<Map<String, Object>> selectPageByJson(String json);
+    List<Map<String, Object>> selectBySql(String sql);
 }
