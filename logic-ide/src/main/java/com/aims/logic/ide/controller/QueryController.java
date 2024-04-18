e64afb9eb91d32f67fe185dca3602344fd22ef8c
lk <442969153@qq.com>
2024-04-18 16:50:04 +0800

    feat: 增加data-model-core依赖进行复杂查询

--- logic-ide/src/main/java/com/aims/logic/ide/controller/QueryController.java
+++ logic-ide/src/main/java/com/aims/logic/ide/controller/QueryController.java
@@ -1,32 +1,33 @@
 package com.aims.logic.ide.controller;
 
+import com.aims.datamodel.core.dsl.DataModel;
+import com.aims.datamodel.core.sqlbuilder.input.QueryInput;
 import com.aims.logic.sdk.dto.ApiResult;
 import com.aims.logic.sdk.service.LogicService;
 import com.alibaba.fastjson2.JSONObject;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/logic/api")
+@RequestMapping("/api/ide/data/{dataModelId}")
 public class QueryController {
     @Autowired
     private LogicService logicService;
 
-    @PostMapping("/query/by-json")
-    public ApiResult<Page<Map<String, Object>>> queryByJson(@RequestBody String json) {
-        return new ApiResult<Page<Map<String, Object>>>().setData(logicService.selectPageByJson(json));
+    @PostMapping("/query")
+    public ApiResult<Page<Map<String, Object>>> queryByJson(@PathVariable("dataModelId") String dataModelId, @RequestBody String json) {
+        QueryInput input = JSONObject.parseObject(json, QueryInput.class);
+        if (input.getFrom() == null)
+            input.setFrom(new DataModel().setMainTable(dataModelId));
+        return new ApiResult<Page<Map<String, Object>>>().setData(logicService.selectPageByInput(input));
     }
 
-    @PostMapping("/query/by-sql")
-    public ApiResult<List<Map<String, Object>>> queryBySql(@RequestBody JSONObject body) {
-        String sql = body.getString("sql");
-        return new ApiResult<List<Map<String, Object>>>().setData(logicService.selectBySql(sql));
-    }
+//    @PostMapping("/queryBySql")
+//    public ApiResult<List<Map<String, Object>>> queryBySql(@RequestBody JSONObject body) {
+//        String sql = body.getString("sql");
+//        return new ApiResult<List<Map<String, Object>>>().setData(logicService.selectBySql(sql));
+//    }
 }
--- logic-sdk/src/main/java/com/aims/logic/sdk/service/BaseService.java
+++ logic-sdk/src/main/java/com/aims/logic/sdk/service/BaseService.java
@@ -1,5 +1,6 @@
 package com.aims.logic.sdk.service;
 
+import com.aims.datamodel.core.sqlbuilder.input.QueryInput;
 import com.aims.logic.sdk.dto.FormQueryInput;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
@@ -10,6 +11,8 @@ import java.util.Map;
 public interface BaseService<T> extends IService<T> {
     Page<T> selectPage(FormQueryInput input);
 
-    Page<Map<String, Object>> selectPageByJson(String json);
+    //    Page<Map<String, Object>> selectPageByJson(String json);
+    Page<Map<String, Object>> selectPageByInput(QueryInput input);
+
     List<Map<String, Object>> selectBySql(String sql);
 }
