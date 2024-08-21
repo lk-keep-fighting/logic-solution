dfc8da38e54ac64e6ceed85ac252d6d97c5fbf96
liukun <442969153@qq.com>
2024-08-21 15:57:37 +0800

    feat(0.1.4): 插入时支持获取自增主键

--- logic-sdk/src/main/java/com/aims/logic/sdk/annotation/IdType.java
+++ logic-sdk/src/main/java/com/aims/logic/sdk/annotation/IdType.java
@@ -2,7 +2,8 @@ package com.aims.logic.sdk.annotation;
 
 public enum IdType {
     NOT_SET("NOT_SET"),
-    ASSIGN_ID("ASSIGN_ID");
+    ASSIGN_ID("ASSIGN_ID"),
+    UUID("UUID");
 
     private String type;
 
