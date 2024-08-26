4cc76e2a7807e96daf73dbb4c90c6fb2aee724e0
liukun <442969153@qq.com>
2024-08-26 14:17:46 +0800

    style(0.1.5): 左侧工具栏节点行高调整为80

--- logic-runtime/src/main/java/com/aims/logic/runtime/env/LogicAppConfig.java
+++ logic-runtime/src/main/java/com/aims/logic/runtime/env/LogicAppConfig.java
@@ -2,11 +2,10 @@ package com.aims.logic.runtime.env;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
 
 @Configuration
 public class LogicAppConfig {
-    @Value("${logic.config-dir}")
+    @Value("${logic.config-dir:./logic-configs}")
     public String CONFIG_DIR;
+
 }
