14afa9d96d98dfc06d4ad0116e0563cef1e1de10
liukun <442969153@qq.com>
2024-10-23 18:02:27 +0800

    fix(0.2.6): /logic路由无法访问

--- logic-ide/src/main/java/com/aims/logic/ide/configuration/LogicStaticResourceConfig.java
+++ logic-ide/src/main/java/com/aims/logic/ide/configuration/LogicStaticResourceConfig.java
@@ -2,12 +2,20 @@ package com.aims.logic.ide.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
-public class LogicStaticResourceConfig implements WebMvcConfigurer {
+public class LogicStaticResourceConfig extends WebMvcConfigurationSupport {
+    @Override
+    protected void addViewControllers(ViewControllerRegistry registry) {
+        super.addViewControllers(registry);
+        registry.addRedirectViewController("/logic", "/logic/index.html");
+    }
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        super.addResourceHandlers(registry);
         registry.addResourceHandler("/logic/**")
                 .addResourceLocations("classpath:/public/");
     }
