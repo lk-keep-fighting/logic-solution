78c8f057793dd1fd94894e21d7d11f551f8347fa
liukun <442969153@qq.com>
2024-11-26 10:48:00 +0800

    fix(0.3.4): 通过父pom统一管理版本

--- logic-ide-service/src/main/java/com/aims/logic/service/controller/StartupController.java
+++ logic-ide-service/src/main/java/com/aims/logic/service/controller/StartupController.java
@@ -3,21 +3,19 @@ package com.aims.logic.service.controller;
 import com.aims.logic.runtime.util.RuntimeUtil;
 import com.alibaba.fastjson2.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
+@RestController
 public class StartupController {
 
-    @GetMapping("/api")
-    public String hello() {
-        return "逻辑编排 java-runtime";
-    }
-    @GetMapping("/health")
-    public JSONObject isOk() {
-        return new JSONObject();
+    @GetMapping("/hello")
+    public String isOk() {
+        return "hi,i'm xiao qiang.";
     }
 
     /**
      * 查看环境变量配置
+     *
      * @return 环境变量json
      */
     @GetMapping("/env")
