cb94ccf08d148d2290851e91992fe3fbaaea2afe
liukun <442969153@qq.com>
2024-11-29 17:11:57 +0800

    feat(0.4.0): 删除ide-service多余文件

--- logic-ide-service/src/main/java/com/aims/logic/service/LogicIdeServiceApplication.java
+++ logic-ide-service/src/main/java/com/aims/logic/service/LogicIdeServiceApplication.java
@@ -1,15 +1,12 @@
 package com.aims.logic.service;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"com.aims.logic", "com.aims.datamodel"})
-@MapperScan("com.aims.logic.service.demo.mapper")
 @SpringBootConfiguration
 public class LogicIdeServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(LogicIdeServiceApplication.class, args);
     }
