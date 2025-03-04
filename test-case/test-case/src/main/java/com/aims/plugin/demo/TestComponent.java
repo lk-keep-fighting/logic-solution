ba26964eea379c91764961cd17a35af8647a561d
刘锟 <liukun@aimstek.cn>
2025-03-04 13:59:32 +0800

    feat(0.6.2.25030401): 新增删除日志方法，可删除n天之前的日志

--- test-case/test-case/src/main/java/com/aims/plugin/demo/TestComponent.java
+++ test-case/test-case/src/main/java/com/aims/plugin/demo/TestComponent.java
@@ -1,5 +1,6 @@
 package com.aims.plugin.demo;
 
+import com.aims.logic.sdk.annotation.LogicItem;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -8,15 +9,13 @@ import java.util.Date;
 
 @Component
 public class TestComponent {
-    //    @LogicItem(name = "输入对象返回对象")
-//    public TestInput func1(TestInput2 input) {
-//        return input.getInputs().stream().max(Comparator.comparingInt(TestInput::getI)).orElse(null);
-//    }
+
     @PostConstruct
     public void init() {
         System.out.println("动态组件 TestComponent 已加载！");
     }
 
+    @LogicItem(name = "测试同名方法2")
     public String strs() {
         return "测试同名方法2";
     }
