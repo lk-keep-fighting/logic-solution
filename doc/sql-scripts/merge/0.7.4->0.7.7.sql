e49cd8fa7076db39d523a88ceb5297e62db33e14
刘锟 <liukun@aimstek.cn>
2025-04-07 17:53:23 +0800

    doc(0.7.7.25040201):sql

--- doc/sql-scripts/merge/0.7.4->0.7.7.sql
+++ doc/sql-scripts/merge/0.7.4->0.7.7.sql
@@ -1,2 +1,4 @@
 ALTER TABLE `logic_instance`
     ADD COLUMN `isAsync` tinyint(1) NULL DEFAULT 0 COMMENT '是否为异步调起' AFTER `retryTimes`;
+ALTER TABLE `logic_bak`
+    ADD INDEX `logicid_version_idx`(`id` ASC, `version` ASC) USING BTREE;
