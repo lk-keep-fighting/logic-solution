d0aa3d431623ba0c04197d2e40e557fb9fa9c8d8
刘锟 <liukun@aimstek.cn>
2025-05-14 15:54:21 +0800

    feat(0.8.0.25050701-SNAPSHOT)- 新增逻辑开始前和停止后的回调

--- test-suite/src/main/java/com/aims/logic/testsuite/demo/event/LogicRunnerEventHandler.java
+++ test-suite/src/main/java/com/aims/logic/testsuite/demo/event/LogicRunnerEventHandler.java
@@ -6,9 +6,22 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class LogicRunnerEventHandler implements LogicRunnerEventListener {
+
+    @Override
+    public void beforeLogicRun(String logicId, String bizId, Object params) {
+        System.out.println("beforeLogicRun: logicId=" + logicId +
+                ", bizId=" + bizId + ", params=" + JSON.toJSONString(params));
+    }
+
+    @Override
+    public void afterLogicStop(String logicId, String bizId, Object returnData) {
+        System.out.println("afterLogicStop: logicId=" + logicId +
+                ", bizId=" + bizId + ", returnData=" + JSON.toJSONString(returnData));
+    }
+
     @Override
     public void onBizCompleted(String logicId, String bizId, Object returnData) {
-        System.out.println("OnBizCompleted: logicId=" + logicId +
+        System.out.println("onBizCompleted: logicId=" + logicId +
                 ", bizId=" + bizId + ", returnData=" + JSON.toJSONString(returnData));
     }
 }
