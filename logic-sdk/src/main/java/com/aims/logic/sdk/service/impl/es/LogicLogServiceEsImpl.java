2c8f3401281e02d14f9f52ba15be5a6413f28782
刘锟 <liukun@aimstek.cn>
2025-06-19 10:21:29 +0800

    feat(0.8.2.260618-SNAPSHOT)：es清空日志时初始化索引

--- logic-sdk/src/main/java/com/aims/logic/sdk/service/impl/es/LogicLogServiceEsImpl.java
+++ logic-sdk/src/main/java/com/aims/logic/sdk/service/impl/es/LogicLogServiceEsImpl.java
@@ -3,17 +3,177 @@ package com.aims.logic.sdk.service.impl.es;
 import com.aims.logic.sdk.entity.LogicLogEntity;
 import com.aims.logic.sdk.service.LogicLogService;
 import com.aims.logic.sdk.service.impl.BaseEsServiceImpl;
-
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import lombok.extern.slf4j.Slf4j;
+import okhttp3.*;
 
 //@Component
 //@ConditionalOnLogicLogService("es")
+@Slf4j
 public class LogicLogServiceEsImpl extends BaseEsServiceImpl<LogicLogEntity, String> implements LogicLogService {
 
-    @Override
-    public void clearLog() {
+    /**
+     * 构建ES索引
+     */
+    private void initEsIndex() {
+        try {
+            deleteEsIndex();
+        } catch (Exception e) {
+            log.error("初始化时删除索引失败");
+            e.printStackTrace();
+        }
+        String bodyStr = "{\n" +
+                "  \"mappings\": {\n" +
+                "    \"properties\": {\n" +
+                "      \"bizId\": {\n" +
+                "        \"type\": \"text\",\n" +
+                "        \"fields\": {\n" +
+                "          \"keyword\": {\n" +
+                "            \"type\": \"keyword\",\n" +
+                "            \"ignore_above\": 256\n" +
+                "          }\n" +
+                "        }\n" +
+                "      },\n" +
+                "      \"env\": {\n" +
+                "        \"type\": \"text\",\n" +
+                "        \"fields\": {\n" +
+                "          \"keyword\": {\n" +
+                "            \"type\": \"keyword\",\n" +
+                "            \"ignore_above\": 256\n" +
+                "          }\n" +
+                "        }\n" +
+                "      },\n" +
+                "      \"host\": {\n" +
+                "        \"type\": \"text\",\n" +
+                "        \"fields\": {\n" +
+                "          \"keyword\": {\n" +
+                "            \"type\": \"keyword\",\n" +
+                "            \"ignore_above\": 256\n" +
+                "          }\n" +
+                "        }\n" +
+                "      },\n" +
+                "      \"id\": {\n" +
+                "        \"type\": \"text\",\n" +
+                "        \"fields\": {\n" +
+                "          \"keyword\": {\n" +
+                "            \"type\": \"keyword\",\n" +
+                "            \"ignore_above\": 256\n" +
+                "          }\n" +
+                "        }\n" +
+                "      },\n" +
+                "      \"isOver\": {\n" +
+                "        \"type\": \"boolean\"\n" +
+                "      },\n" +
+                "      \"itemLogs\": {\n" +
+                "        \"type\": \"text\",\n" +
+                "        \"fields\": {\n" +
+                "          \"keyword\": {\n" +
+                "            \"type\": \"keyword\",\n" +
+                "            \"ignore_above\": 256\n" +
+                "          }\n" +
+                "        }\n" +
+                "      },\n" +
+                "      \"logicId\": {\n" +
+                "        \"type\": \"text\",\n" +
+                "        \"fields\": {\n" +
+                "          \"keyword\": {\n" +
+                "            \"type\": \"keyword\",\n" +
+                "            \"ignore_above\": 256\n" +
+                "          }\n" +
+                "        }\n" +
+                "      },\n" +
+                "      \"messageId\": {\n" +
+                "        \"type\": \"text\",\n" +
+                "        \"fields\": {\n" +
+                "          \"keyword\": {\n" +
+                "            \"type\": \"keyword\",\n" +
+                "            \"ignore_above\": 256\n" +
+                "          }\n" +
+                "        }\n" +
+                "      },\n" +
+                "      \"paramsJson\": {\n" +
+                "        \"type\": \"text\",\n" +
+                "        \"fields\": {\n" +
+                "          \"keyword\": {\n" +
+                "            \"type\": \"keyword\",\n" +
+                "            \"ignore_above\": 256\n" +
+                "          }\n" +
+                "        }\n" +
+                "      },\n" +
+                "      \"returnData\": {\n" +
+                "        \"type\": \"text\",\n" +
+                "        \"fields\": {\n" +
+                "          \"keyword\": {\n" +
+                "            \"type\": \"keyword\",\n" +
+                "            \"ignore_above\": 256\n" +
+                "          }\n" +
+                "        }\n" +
+                "      },\n" +
+                "      \"serverTime\": {\n" +
+                "        \"type\": \"date\",\n" +
+                "        \"format\": \"yyyy-MM-dd HH:mm:ss.SSS||yyyy-MM-dd HH:mm:ss||epoch_millis\",\n" +
+                "        \"fields\": {\n" +
+                "          \"keyword\": {\n" +
+                "            \"type\": \"keyword\",\n" +
+                "            \"ignore_above\": 256\n" +
+                "          }\n" +
+                "        }\n" +
+                "      },\n" +
+                "      \"success\": {\n" +
+                "        \"type\": \"boolean\"\n" +
+                "      },\n" +
+                "      \"varsJson\": {\n" +
+                "        \"type\": \"text\",\n" +
+                "        \"fields\": {\n" +
+                "          \"keyword\": {\n" +
+                "            \"type\": \"keyword\",\n" +
+                "            \"ignore_above\": 256\n" +
+                "          }\n" +
+                "        }\n" +
+                "      },\n" +
+                "      \"varsJsonEnd\": {\n" +
+                "        \"type\": \"text\",\n" +
+                "        \"fields\": {\n" +
+                "          \"keyword\": {\n" +
+                "            \"type\": \"keyword\",\n" +
+                "            \"ignore_above\": 256\n" +
+                "          }\n" +
+                "        }\n" +
+                "      },\n" +
+                "      \"version\": {\n" +
+                "        \"type\": \"text\",\n" +
+                "        \"fields\": {\n" +
+                "          \"keyword\": {\n" +
+                "            \"type\": \"keyword\",\n" +
+                "            \"ignore_above\": 256\n" +
+                "          }\n" +
+                "        }\n" +
+                "      }\n" +
+                "    }\n" +
+                "  }\n" +
+                "}";
+        OkHttpClient client = new OkHttpClient();
+        try {
+            Request request = new Request.Builder()
+                    .url(esHost + "/" + indexName)
+                    .put(RequestBody.create(MediaType.parse("application/json"), bodyStr))
+                    .build();
+
+            // 执行请求
+            Response response = client.newCall(request).execute();
+
+            if (!response.isSuccessful()) {
+                throw new RuntimeException("初始化日志失败: " + response.body().string());
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException("初始化ES日志时发生错误", e);
+        }
+    }
+
+    /**
+     * 清除ES索引
+     */
+    private void deleteEsIndex() {
         // 构建OkHttpClient
         OkHttpClient client = new OkHttpClient();
 
@@ -37,8 +197,52 @@ public class LogicLogServiceEsImpl extends BaseEsServiceImpl<LogicLogEntity, Str
     }
 
     @Override
+    public void clearLog() {
+        //初始化ES索引
+        initEsIndex();
+        // 构建OkHttpClient
+        OkHttpClient client = new OkHttpClient();
+
+        try {
+            // 构建DELETE请求
+            Request request = new Request.Builder()
+                    .url(esHost + "/" + indexName + "/_delete_by_query")
+                    .post(RequestBody.create(MediaType.parse("application/json"), "{\"query\":{\"match_all\":{}}}}"))
+                    .build();
+
+            // 执行请求
+            Response response = client.newCall(request).execute();
+
+            if (!response.isSuccessful()) {
+                throw new RuntimeException("清除ES日志失败: " + response.body().string());
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException("清除ES日志时发生错误", e);
+        }
+    }
+
+    @Override
     public void deleteLogBeforeDays(int days) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deleteLogBeforeDays'");
+        // 构建OkHttpClient
+        OkHttpClient client = new OkHttpClient();
+
+        try {
+            // 构建DELETE请求
+            Request request = new Request.Builder()
+                    .url(esHost + "/" + indexName + "/_delete_by_query")
+                    .post(RequestBody.create(MediaType.parse("application/json"), "{\"query\":{\"range\":{\"createTime\":{\"lte\":\"now-" + days + "d/d\"}}}}"))
+                    .build();
+
+            // 执行请求
+            Response response = client.newCall(request).execute();
+
+            if (!response.isSuccessful()) {
+                throw new RuntimeException("清除ES日志失败: " + response.body().string());
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException("清除ES日志时发生错误", e);
+        }
     }
 }
