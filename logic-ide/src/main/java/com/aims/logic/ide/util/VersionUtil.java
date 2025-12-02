f60181f76979634f1b93dccf66e91c33b2a66c25
lk <442969153@qq.com>
2025-12-02 17:19:58 +0800

    feat:redisson包改为option=true

--- logic-ide/src/main/java/com/aims/logic/ide/util/VersionUtil.java
+++ logic-ide/src/main/java/com/aims/logic/ide/util/VersionUtil.java
@@ -2,6 +2,6 @@ package com.aims.logic.ide.util;
 
 public class VersionUtil {
     public static String getVersion() {
-        return "0.10.2-SNAPSHOT";
+        return "0.10.3-SNAPSHOT";
     }
 }
